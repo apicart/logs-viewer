a3c99c1ca930aff1844b3836f2fb90e7b2980b9c
Vladimir Machacek <8machy@seznam.cz>
2019-08-22 21:09:23 +0200

    Moved to vue.js

--- public/assets/js/modules/logs-viewer.js
+++ public/assets/js/modules/logs-viewer.js
@@ -1,74 +1,82 @@
-Brackets
-	.addComponent('logsItem', {
-		instanceId: 'logsItem',
-		data: {
+Vue.component('logs-list', {
+	props: ['logs'],
+	template: '#logs-list-template'
+});
+
+Vue.component('log-item', {
+	props: ['log'],
+	data () {
+		return {
 			toggled: false,
-			deleted: false
-		},
-		methods: {
-			showNestedLogs: function (event, filePath) {
-				this.toggled = ! this.toggled;
-
-				if ( ! this.log.nestedLogs.length) {
-					var
-						self = this,
-						log = this.log;
-
-					Utils.ajax({
-						url: window.getLogsUrl,
-						data: {
-							directoryPath: filePath
-						},
-						start: function () {
-							document.body.style.cursor='wait';
-						},
-						complete: function (response) {
-							log.nestedLogs = Utils.json.parse(response.data);
-							self.log = log;
-							document.body.style.cursor='default';
-						}
-					});
-				}
-			},
-			deleteLogFile: function (event, filePath) {
-				var self = this;
-				if (confirm('File: "' + filePath + '" will be deleted')) {
-					Utils.ajax({
-						url: window.deleteLogUrl,
-						data: {
-							filePath: filePath
-						},
-						start: function () {
-							document.body.style.cursor='wait';
-						},
-						complete: function (response) {
-							self.deleted = true;
-							document.body.style.cursor='default';
-						}
-					});
-				}
-			}
-		},
-		afterRender: function () {
-			if (this.data.deleted) {
-				document.querySelector(this.el).remove();
-			}
-		},
-		template: '#logs-list-item-template'
-	})
-	.addComponent('logsList', {
-		instanceId: 'logsList',
-		template: '#logs-list-template'
-	})
-	.render({
-		instanceId: 'logs-viewer',
-		el: '#logs-list',
-		data: {
-			logs: JSON.parse(window.logs)
-		},
-		template: function () {
-			return this.data.logs.length
-				? '{{component logsList, logs: logs, toggled: true}}'
-				: '<h1>No logs found.</h1>'
+			deleted: false,
+			viewLogUrl: window.viewLogUrl
 		}
+	},
+	methods: {
+		showNestedLogs(filePath) {
+			this.toggled = ! this.toggled;
+
+			if ( ! this.log.nestedLogs.length) {
+				let
+					self = this,
+					log = self.log;
+
+				Utils.ajax({
+					url: window.getLogsUrl,
+					data: {
+						directoryPath: filePath
+					},
+					start: function () {
+						document.body.style.cursor = 'wait';
+					},
+					complete: function (response) {
+						log.nestedLogs = Utils.json.parse(response.data);
+						self.log = log;
+						document.body.style.cursor = 'default';
+					}
+				});
+			}
+		},
+		deleteLogFile(filePath) {
+			var self = this;
+
+			if (confirm('File: "' + filePath + '" will be deleted')) {
+				Utils.ajax({
+					url: window.deleteLogUrl,
+					data: {
+						filePath: filePath
+					},
+					start: function () {
+						document.body.style.cursor = 'wait';
+					},
+					complete: function (response) {
+						self.deleted = true;
+						document.body.style.cursor = 'default';
+					}
+				});
+			}
+		}
+	},
+	template: '#logs-list-item-template'
+})
+
+
+let logs = Utils.json.parse(window.logs);
+
+if (logs.length) {
+	new Vue({
+		el: '#logs-list',
+		data () {
+			return {
+				logs: logs
+			}
+		},
+		template: '#logs-list-template'
 	});
+
+} else {
+	new Vue({
+		el: '#logs-list',
+		template: '<h1 class="container text-center">No logs found.</h1>'
+	});
+}
